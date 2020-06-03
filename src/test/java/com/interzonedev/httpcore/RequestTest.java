0758c042ad53b83f5274957d899c5a82b20afc25
Mark Markarian <mark@interzoendev.com>
2020-06-02 21:21:50 -0700

    Updated dependencies. Added the scm element to the POM. Adopted JUnit Jupiter.

--- src/test/java/com/interzonedev/httpcore/RequestTest.java
+++ src/test/java/com/interzonedev/httpcore/RequestTest.java
@@ -1,18 +1,17 @@
 package com.interzonedev.httpcore;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RequestTest {
-
-    private static final Logger log = (Logger) LoggerFactory.getLogger(RequestTest.class);
 
     private final String testUrl = "http://example.com?qk1=qv1";
     private final Method testMethod = Method.POST;
@@ -21,85 +20,65 @@ public class RequestTest {
     private final String testBody = "test body";
 
     {
-        testHeaders.put("hk1", Arrays.asList(new String[] { "hv11", "hv12" }));
-        testHeaders.put("hk2", Arrays.asList(new String[] { "hv21" }));
+        testHeaders.put("hk1", Arrays.asList(new String[]{"hv11", "hv12"}));
+        testHeaders.put("hk2", Arrays.asList(new String[]{"hv21"}));
 
-        testParameters.put("pk1", Arrays.asList(new String[] { "pv11", "pv12" }));
-        testParameters.put("pk2", Arrays.asList(new String[] { "pv21" }));
+        testParameters.put("pk1", Arrays.asList(new String[]{"pv11", "pv12"}));
+        testParameters.put("pk2", Arrays.asList(new String[]{"pv21"}));
     }
 
     @Test
     public void testDefaultValues() {
-        log.debug("testDefaultValues: Start");
-
         Request testRequest = Request.newBuilder().build();
 
-        Assert.assertNull(testRequest.getUrl());
-        Assert.assertNull(testRequest.getMethod());
-        Assert.assertTrue(testRequest.getHeaders().isEmpty());
-        Assert.assertTrue(testRequest.getParameters().isEmpty());
-        Assert.assertNull(testRequest.getBody());
-
-        log.debug("testDefaultValues: End");
+        assertNull(testRequest.getUrl());
+        assertNull(testRequest.getMethod());
+        assertTrue(testRequest.getHeaders().isEmpty());
+        assertTrue(testRequest.getParameters().isEmpty());
+        assertNull(testRequest.getBody());
     }
 
     @Test
     public void testSetValues() {
-        log.debug("testSetValues: Start");
-
         Request testRequest = Request.newBuilder().setUrl(testUrl).setMethod(testMethod).setHeaders(testHeaders)
                 .setParameters(testParameters).setBody(testBody).build();
 
-        Assert.assertEquals(testUrl, testRequest.getUrl());
-        Assert.assertEquals(testMethod, testRequest.getMethod());
-        Assert.assertEquals(testHeaders, testRequest.getHeaders());
-        Assert.assertEquals(testParameters, testRequest.getParameters());
-        Assert.assertEquals(testBody, testRequest.getBody());
-
-        log.debug("testSetValues: End");
+        assertEquals(testUrl, testRequest.getUrl());
+        assertEquals(testMethod, testRequest.getMethod());
+        assertEquals(testHeaders, testRequest.getHeaders());
+        assertEquals(testParameters, testRequest.getParameters());
+        assertEquals(testBody, testRequest.getBody());
     }
 
     @Test
     public void testClonedValues() {
-        log.debug("testClonedValues: Start");
-
         Request testRequest = Request.newBuilder().setUrl(testUrl).setMethod(testMethod).setHeaders(testHeaders)
                 .setParameters(testParameters).setBody(testBody).build();
         Request clonedRequest = Request.newBuilder(testRequest).build();
 
-        Assert.assertEquals(testUrl, clonedRequest.getUrl());
-        Assert.assertEquals(testMethod, clonedRequest.getMethod());
-        Assert.assertEquals(testHeaders, clonedRequest.getHeaders());
-        Assert.assertEquals(testParameters, clonedRequest.getParameters());
-        Assert.assertEquals(testBody, clonedRequest.getBody());
-
-        log.debug("testClonedValues: End");
+        assertEquals(testUrl, clonedRequest.getUrl());
+        assertEquals(testMethod, clonedRequest.getMethod());
+        assertEquals(testHeaders, clonedRequest.getHeaders());
+        assertEquals(testParameters, clonedRequest.getParameters());
+        assertEquals(testBody, clonedRequest.getBody());
     }
 
     @Test
     public void testSetNullHeaders() {
-        log.debug("testSetNullHeaders: Start");
-
         Request.Builder testRequestBuilder = Request.newBuilder().setHeaders(testHeaders);
         testRequestBuilder.setHeaders(null);
         Request testRequest = testRequestBuilder.build();
 
-        Assert.assertTrue(testRequest.getHeaders().isEmpty());
-
-        log.debug("testSetNullHeaders: End");
+        assertTrue(testRequest.getHeaders().isEmpty());
     }
 
     @Test
     public void testSetNullParameters() {
-        log.debug("testSetNullParameters: Start");
-
         Request.Builder testRequestBuilder = Request.newBuilder().setParameters(testParameters);
         testRequestBuilder.setParameters(null);
         Request testRequest = testRequestBuilder.build();
 
-        Assert.assertTrue(testRequest.getParameters().isEmpty());
-
-        log.debug("testSetNullParameters: End");
+        assertTrue(testRequest.getParameters().isEmpty());
     }
 
 }
--- src/test/java/com/interzonedev/httpcore/ResponseTest.java
+++ src/test/java/com/interzonedev/httpcore/ResponseTest.java
@@ -1,21 +1,19 @@
 package com.interzonedev.httpcore;
 
+import org.junit.jupiter.api.Test;
+
+import javax.servlet.http.Cookie;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
-import javax.servlet.http.Cookie;
-
-import org.junit.Assert;
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ResponseTest {
-
-    private static final Logger log = (Logger) LoggerFactory.getLogger(ResponseTest.class);
 
     private final Request testRequest = Request.newBuilder().build();
     private final int testStatus = 200;
@@ -27,8 +25,8 @@ public class ResponseTest {
     private final Locale testLocale = Locale.US;
 
     {
-        testHeaders.put("hk1", Arrays.asList(new String[] { "hv11", "hv12" }));
-        testHeaders.put("hk2", Arrays.asList(new String[] { "hv21" }));
+        testHeaders.put("hk1", Arrays.asList(new String[]{"hv11", "hv12"}));
+        testHeaders.put("hk2", Arrays.asList(new String[]{"hv21"}));
 
         testCookies.put("c1", new Cookie("cn1", "cv1"));
         testCookies.put("c2", new Cookie("cn2", "cv2"));
@@ -36,87 +34,67 @@ public class ResponseTest {
 
     @Test
     public void testDefaultValues() {
-        log.debug("testDefaultValues: Start");
-
         Response testResponse = Response.newBuilder().build();
 
-        Assert.assertNull(testResponse.getRequest());
-        Assert.assertEquals(0, testResponse.getStatus());
-        Assert.assertNull(testResponse.getContentType());
-        Assert.assertEquals(0, testResponse.getContentLength());
-        Assert.assertTrue(testResponse.getHeaders().isEmpty());
-        Assert.assertTrue(testResponse.getCookies().isEmpty());
-        Assert.assertNull(testResponse.getContent());
-        Assert.assertNull(testResponse.getLocale());
-
-        log.debug("testDefaultValues: End");
+        assertNull(testResponse.getRequest());
+        assertEquals(0, testResponse.getStatus());
+        assertNull(testResponse.getContentType());
+        assertEquals(0, testResponse.getContentLength());
+        assertTrue(testResponse.getHeaders().isEmpty());
+        assertTrue(testResponse.getCookies().isEmpty());
+        assertNull(testResponse.getContent());
+        assertNull(testResponse.getLocale());
     }
 
     @Test
     public void testSetValues() {
-        log.debug("testSetValues: Start");
-
         Response testResponse = Response.newBuilder().setRequest(testRequest).setStatus(testStatus)
                 .setContentType(testContentType).setContentLength(testContentLength).setHeaders(testHeaders)
                 .setCookies(testCookies).setContent(testContent).setLocale(testLocale).build();
 
-        Assert.assertEquals(testRequest, testResponse.getRequest());
-        Assert.assertEquals(testStatus, testResponse.getStatus());
-        Assert.assertEquals(testContentType, testResponse.getContentType());
-        Assert.assertEquals(testContentLength, testResponse.getContentLength());
-        Assert.assertEquals(testHeaders, testResponse.getHeaders());
-        Assert.assertEquals(testCookies, testResponse.getCookies());
-        Assert.assertEquals(testContent, testResponse.getContent());
-        Assert.assertEquals(testLocale, testResponse.getLocale());
-
-        log.debug("testSetValues: End");
+        assertEquals(testRequest, testResponse.getRequest());
+        assertEquals(testStatus, testResponse.getStatus());
+        assertEquals(testContentType, testResponse.getContentType());
+        assertEquals(testContentLength, testResponse.getContentLength());
+        assertEquals(testHeaders, testResponse.getHeaders());
+        assertEquals(testCookies, testResponse.getCookies());
+        assertEquals(testContent, testResponse.getContent());
+        assertEquals(testLocale, testResponse.getLocale());
     }
 
     @Test
     public void testClonedValues() {
-        log.debug("testClonedValues: Start");
-
         Response testResponse = Response.newBuilder().setRequest(testRequest).setStatus(testStatus)
                 .setContentType(testContentType).setContentLength(testContentLength).setHeaders(testHeaders)
                 .setCookies(testCookies).setContent(testContent).setLocale(testLocale).build();
         Response clonedResponse = Response.newBuilder(testResponse).build();
 
-        Assert.assertEquals(testRequest, clonedResponse.getRequest());
-        Assert.assertEquals(testStatus, clonedResponse.getStatus());
-        Assert.assertEquals(testContentType, clonedResponse.getContentType());
-        Assert.assertEquals(testContentLength, clonedResponse.getContentLength());
-        Assert.assertEquals(testHeaders, clonedResponse.getHeaders());
-        Assert.assertEquals(testCookies, clonedResponse.getCookies());
-        Assert.assertEquals(testContent, clonedResponse.getContent());
-        Assert.assertEquals(testLocale, clonedResponse.getLocale());
-
-        log.debug("testClonedValues: End");
+        assertEquals(testRequest, clonedResponse.getRequest());
+        assertEquals(testStatus, clonedResponse.getStatus());
+        assertEquals(testContentType, clonedResponse.getContentType());
+        assertEquals(testContentLength, clonedResponse.getContentLength());
+        assertEquals(testHeaders, clonedResponse.getHeaders());
+        assertEquals(testCookies, clonedResponse.getCookies());
+        assertEquals(testContent, clonedResponse.getContent());
+        assertEquals(testLocale, clonedResponse.getLocale());
     }
 
     @Test
     public void testSetNullHeaders() {
-        log.debug("testSetNullHeaders: Start");
-
         Response.Builder testResponseBuilder = Response.newBuilder().setHeaders(testHeaders);
         testResponseBuilder.setHeaders(null);
         Response testResponse = testResponseBuilder.build();
 
-        Assert.assertTrue(testResponse.getHeaders().isEmpty());
-
-        log.debug("testSetNullHeaders: End");
+        assertTrue(testResponse.getHeaders().isEmpty());
     }
 
     @Test
     public void testSetNullCookies() {
-        log.debug("testSetNullCookies: Start");
-
         Response.Builder testResponseBuilder = Response.newBuilder().setCookies(testCookies);
         testResponseBuilder.setCookies(null);
         Response testResponse = testResponseBuilder.build();
 
-        Assert.assertTrue(testResponse.getCookies().isEmpty());
-
-        log.debug("testSetNullCookies: End");
+        assertTrue(testResponse.getCookies().isEmpty());
     }
 
 }
